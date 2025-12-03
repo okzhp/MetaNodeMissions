22ca3b885f78abfd74a2b7c201d6db901f0e1c89
zhuhuipeng <zhuhuipeng@wande.com>
2025-12-03 15:05:56 +0800

    submit go mission2

--- Golang开发基础/mission2/pointer.go
+++ Golang开发基础/mission2/pointer.go
@@ -4,15 +4,15 @@ import "fmt"
 
 // 指针
 func main() {
-  num := 100
-  addTen(&num)
-  fmt.Println("num 100,after addTen:", num)
+	num := 100
+	addTen(&num)
+	fmt.Println("num 100,after addTen:", num)
 
-  fmt.Println("===========================================")
+	fmt.Println("===========================================")
 
-  nums := []int{1, 2, 3}
-  multiply2(&nums)
-  fmt.Println("nums[1,2,3] after multiply2:", nums)
+	nums := []int{1, 2, 3}
+	multiply2(&nums)
+	fmt.Println("nums[1,2,3] after multiply2:", nums)
 
 }
 
@@ -20,13 +20,13 @@ func main() {
 // 编写一个Go程序，定义一个函数，该函数接收一个整数指针作为参数，
 // 在函数内部将该指针指向的值增加10，然后在主函数中调用该函数并输出修改后的值。
 func addTen(p *int) {
-  *p += 10
+	*p += 10
 }
 
 // 题目 ：
 // 实现一个函数，接收一个整数切片的指针，将切片中的每个元素乘以2。
 func multiply2(nums *[]int) {
-  for i, _ := range *nums {
-    (*nums)[i] *= 2
-  }
+	for i, _ := range *nums {
+		(*nums)[i] *= 2
+	}
 }
